f5160c0bdb0239a184dee660bf9d5c0909038df6
student08 <praetawan9925@snru.ac.th>
2017-06-13 15:16:23 +0700

    If

---
+++ If else Statement.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var intAge = 55
+
+
+if intAge > 50 {
+    print("You are old")
+}else{
+    print("You are young")
+}
+//การใช้ if else
+
+
---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Optional คืออะไร คือ การอนุญาตให้มีค่าว่างได้
+var intNumber: Int?  //การกำหนดให้มีค่าว่าง
+
+//print("intNumber ==> \(intNumber)")
+
+//แนวทางการใช้ optional 
+
+var strNumber = "5"
+
+strNumber = "10"
+//strNumber = "ten"
+
+var intNumber1 = Int(strNumber)
+if var intMyNumber = intNumber1  {
+    var myAnswer = intNumber
+    print("myAnswer * 2 ==> /(myAnswer * 2)")
+    
+}
+
+
+
+
